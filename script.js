08b52efec6bf0a14a3d07758dc27ad007e2ece6c
Aleandro <aleceraus@gmail.com>
2025-04-15 22:15:15 +0200

    email emergency fix

--- script.js
+++ script.js
@@ -18,7 +18,8 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
     
-    document.querySelector("form").addEventListener("submit", function(event) {
+    // emergency fix
+    /*document.querySelector("form").addEventListener("submit", function(event) {
         event.preventDefault();
         
         let name = document.getElementById("nameSender").value;
@@ -31,14 +32,13 @@ document.addEventListener("DOMContentLoaded", function() {
         let recipient = user + "@" + domain;
             
         let mailtoLink = `mailto:${recipient}?subject=Messaggio da ${name}&body=${encodeURIComponent(message)}%0A%0ADa: ${name} (${email})`;
-        
         window.location.href = mailtoLink;
 
         // Fast way to clean form after submit pressed
         setTimeout(() => {
             document.querySelector("form").reset();
         }, 500);
-    });
+    });*/
 });
 
 // Inspired by DJango 'include' feature
@@ -175,5 +175,29 @@ document.addEventListener("mousemove", (e) => {
 /* Testing utilities */
 function testCalling(){
     console.log("It has been called")
+}
+
+// emergency fix
+function listenSubmitEmain() {
+    document.querySelector("form").addEventListener("submit", function(event) {
+        event.preventDefault();
+        
+        let name = document.getElementById("nameSender").value;
+        let email = document.getElementById("exampleInputEmail").value;
+        let message = document.getElementById("messageInput").value;
+    
+        // Light security check, obfuscation - not having backend to do so - btw it doesn't worth it to stole it
+        let user = "alceblockdev";
+        let domain = "gmail.com";
+        let recipient = user + "@" + domain;
+            
+        let mailtoLink = `mailto:${recipient}?subject=Messaggio da ${name}&body=${encodeURIComponent(message)}%0A%0ADa: ${name} (${email})`;
+        window.location.href = mailtoLink;
+    
+        // Fast way to clean form after submit pressed
+        setTimeout(() => {
+            document.querySelector("form").reset();
+        }, 500);
+    });
 }
 
